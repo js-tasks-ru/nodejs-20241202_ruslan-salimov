4efb901be70bab9b77e97d486247ae7f0a3a66ae
RuslanSalimov <mail-spam2012@yandex.ru>
2024-12-19 00:47:42 +0500

    add solution for 1-module task-controller

---
+++ 02-nestjs-basics/01-tasks-controller/tasks/task-status.enum.ts
@@ -0,0 +1,8 @@
+export const enum TaskErrors {
+    TASK_NOT_FOUND = 'Задачи с таким идентификатором не найдено',
+    TASK_TITLE_INCORRECT = 'Заголовок задачи не установлен',
+    TASK_DESCRIPTION_INCORRECT = 'Описание задачи не установлено',
+    TASK_STATUS_INCORRECT = 'Статус задачи должен быть в множестве TaskStatus',
+
+
+}
