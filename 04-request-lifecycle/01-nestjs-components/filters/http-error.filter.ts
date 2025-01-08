304e1a3f6473280a1caf9eac2cac7c0124f223f2
RuslanSalimov <mail-spam2012@yandex.ru>
2025-01-09 00:55:31 +0500

    Исправил HTTPStatus

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,4 +1,4 @@
-import {ArgumentsHost, Catch, ExceptionFilter, HttpException} from "@nestjs/common";
+import {ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus} from "@nestjs/common";
 import { Response } from 'express';
 import {appendFileSync} from 'fs';
 @Catch()
@@ -7,7 +7,7 @@ export class HttpErrorFilter implements ExceptionFilter {
     const message = exception.message;
     const date = new Date().toISOString();
     let response = host.switchToHttp().getResponse<Response>();
-    const status = exception instanceof HttpException ? exception.getStatus() : 500;
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
     appendFileSync("errors.log", `[${date}] ${status} - ${message}\n`);
     response
         .status(status)
